5f49e36ff737d22704e5cdd5d9d5c333b0fc8eb9
Lana Oreoluwa <lanaoreoluwa@Ore.local>
2020-06-17 21:59:53 +0100

    Built a GUI for the game using tkinter

--- rps.py
+++ rps.py
@@ -1,26 +1,66 @@
 import random
+import tkinter
+from tkinter import *
+stats = []
 
-choice = int(input("Please pick and option:\n 1.rock\n 2.paper\n 3.scissors\n"))
+def get_winner(call):
+    choicelist = ['rock','paper','scissors']
+    throw = random.choice(choicelist)
 
-if choice == 1:
-    user_choice = 'Rock'
-elif choice == 2:
-    user_choice = 'Paper'
+    if (throw == 'rock' and call =='paper') or (throw == 'scissors' and call == 'rock') or (throw == 'paper' and call == 'scissors'):
+        stats.append('W')
+        result = "You won!"
+    elif throw == call:
+        stats.append('D')
+        result = "It's a draw"
+    else:
+        stats.append('L')
+        result = "You lost"
+
+    global output
+    output.config(text="Computer did: " + throw + "\n" + result)
+
+def pass_s():
+    get_winner("scissors")
+
+def pass_r():
+    get_winner("rock")
+
+def pass_p():
+    get_winner("paper")
+
+
+window = tkinter.Tk()
+window.geometry("500x250+800+100")
+def end_game():
+    window.destroy()
+
+bottomframe = Frame(window)
+bottomframe.pack( side ="bottom")
+window.title("Rock-Paper-Scissors Game")
+welcome = tkinter.Label(window, width=50, height=5, fg="red", text="Welcome To Boredom Incorporation, Play On!")
+rock = tkinter.Button(window, text="Rock", highlightbackground="#80ff80", fg="black", padx=10, pady=5, command=pass_r, width=20)
+paper = tkinter.Button(window, text="Paper", highlightbackground="#3399ff", padx=10, pady=5, command=pass_p, width=20)
+scissors = tkinter.Button(window, text="Scissors", highlightbackground="#ff9999", padx=10, pady=5, command=pass_s, width=20)
+output = tkinter.Label(window, width=20, fg="red", text="What's your call?")
+gameend = tkinter.Button(bottomframe, text="End game", highlightbackground="black", padx=10, pady=5, command=end_game, width=20)
+
+
+welcome.pack()
+rock.pack(side="left")
+paper.pack(side="left")
+scissors.pack(side="left")
+output.pack(side="right")
+gameend.pack(side="bottom")
+window.mainloop()
+
+for i in stats: print(i, end=" ")
+if stats.count('L') > stats.count('W'):
+    result = "\n You lose the series"
+elif stats.count('L') == stats.count('W'):
+    result = "\n Series ended in a draw"
 else:
-    user_choice = 'Scissors'
+    result = "\n You win the series"
 
-print("Your choice is: " + user_choice + "\nNow its the computer's turn")
-
-comp_list = ['Rock','Paper','Scissors']
-
-comp_choice = random.choice(comp_list)
-
-print("Computer's choice is: " + comp_choice)
-
-if user_choice == comp_choice:
-    print("Its a tie, no winner")
-elif (user_choice == 'Rock' and comp_choice =='Paper') or (user_choice == 'Rock' and comp_choice == 'Scissors') or (user_choice == 'Scissors' and comp_choice == 'Paper'):
-    print("You win!")
-else:
-    print("You lose!")
+print(result)
 
